a48aa0fefb10e97a49a1d65110f69fed33c2ddce
shadickempire <shadjobupdate@gmail.com>
2024-10-17 11:14:07 +0530

    instagram clone

---
+++ script.js
@@ -0,0 +1,7 @@
+// Placeholder for interactive features
+// You can add functionality for likes, comments, and more
+document.querySelectorAll('.icon').forEach(icon => {
+    icon.addEventListener('click', () => {
+        alert("You clicked an action icon!");
+    });
+});
